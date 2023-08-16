40b120531f640227dc815c7e64e494f540dfbf63
Pavel Petkov <elias5247698@abv.bg>
2023-08-17 02:46:22 +0300

    finnaly made separation in modals and make map card sprite in different class

--- sprites/modal.py
+++ sprites/modal.py
@@ -18,80 +18,45 @@ class Modal(TextureGroup):
         window.blit(self.surface, (self.x, self.y))
 
 
-class GenericMapCardModal(Modal):
-    def __init__(self, inner_rect_color, map_card_name, inner_rect_data, price):
-        self.inner_rect_color = inner_rect_color
-        self.inner_rect_data = inner_rect_data
+class MapCard(Texture):
+    def __init__(self, width, height, inner_surface_fill_color, map_card_name, map_card_data, blit_pos):
         super().__init__()
-        inner_rect_x = self.x / 2
-        inner_rect_y = self.y / 4
-        inner_surface = pygame.Surface((self.width - 2 * self.x / 2,
-                                        self.height - 2 * self.y / 4))
-        inner_surface.fill("white")
+        self.width = width
+        self.height = height
+        self.inner_surface_fill_color = inner_surface_fill_color
+        self.map_card_name = map_card_name
+        self.map_card_data = map_card_data
+        self.blit_pos = blit_pos
 
-        self.buttons = [
-            Button(
-                width_and_height_tuple=(self.width / 4, self.height / 12),
-                background="green",
-                text="Да",
-                text_size=20,
-                text_color="black",
-                blit_pos=(0, self.surface.get_height() - self.height / 12)
-            ),
-            Button(
-                width_and_height_tuple=(self.width / 4, self.height / 12),
-                background="red",
-                text="Не",
-                text_size=20,
-                text_color="black",
-                blit_pos=(self.surface.get_width() - self.width / 4, self.surface.get_height() - self.height / 12)
-            )
-        ]
-        for button in self.buttons:
-            button.blit(self.surface)
+        self.surface = pygame.Surface((self.width, self.height))
+        self.surface.fill("white")
+        self.inner_fill_surface = pygame.Surface((2 / 3 * self.width, 1 / 5 * self.height))
+        self.inner_fill_surface.fill(self.inner_surface_fill_color)
+        self.blit_text()
 
-        inner_fill_rect_x = inner_surface.get_width() / 6
-        inner_fill_rect_y = inner_surface.get_height() / 10
-        self.create_text_and_blit(self.surface, f"Искате ли да купите този имот за {price} $", 30, "white",
-                                  (self.width / 2, self.height / 10))
-
-        inner_fill_surface = pygame.Surface((
-            inner_surface.get_width() - 2 * inner_surface.get_width() / 6,
-            inner_surface.get_height() - 2 * 4 * inner_surface.get_height() / 10))
-
-        padding = inner_fill_surface.get_height() / 5
-        padding_counter = inner_fill_surface.get_height() / 3
-
-        inner_fill_surface.fill(self.inner_rect_color)
-
-        for word in map_card_name.split(" "):
-            self.create_text_and_blit(inner_fill_surface, word, 25, (0, 0, 0),
-                                      (inner_fill_surface.get_width() / 2, padding_counter))
+    def blit_text(self):
+        padding = self.inner_fill_surface.get_height() / 5
+        padding_counter = self.inner_fill_surface.get_height() / 3
+        for word in self.map_card_name.split(" "):
+            self.create_text_and_blit(self.inner_fill_surface, word, 25, (0, 0, 0), (self.inner_fill_surface.get_width() / 2, padding_counter))
             padding_counter += padding
+        padding_counter += self.height / 5
+        self.surface.blit(self.inner_fill_surface, (self.width / 6, self.height / 10))
 
-        padding_counter += inner_surface.get_height() / 5
-        for key, value in inner_rect_data.items():
+        for key, value in self.map_card_data.items():
             if key == "0":
-                self.create_text_and_blit(inner_surface, f"Наем: {value} $", 25, (0, 0, 0),
-                                          (inner_surface.get_width() / 2, padding_counter))
-                padding_counter += inner_surface.get_height() / 8
+                self.create_text_and_blit(self.surface, f"Наем: {value} $", 25, (0, 0, 0),
+                                          (self.width / 2, padding_counter))
+                padding_counter += self.height / 8
             else:
-                self.create_text_and_blit(inner_surface, f"{key} {'Къща' if key == '1' else 'Къщи'}", 20, (0, 0, 0),
-                                          (inner_surface.get_width() / 4, padding_counter))
-                self.create_text_and_blit(inner_surface, f"{value} $", 20, (0, 0, 0),
-                                          (inner_surface.get_width() * 3 / 4, padding_counter))
+                self.create_text_and_blit(self.surface, f"{key} {'Къща' if key == '1' else 'Къщи'}", 20, (0, 0, 0),
+                                          (self.width / 4, padding_counter))
+                self.create_text_and_blit(self.surface, f"{value} $", 20, (0, 0, 0),
+                                          (self.width * 3 / 4, padding_counter))
             padding_counter += padding
-
-        inner_surface.blit(inner_fill_surface, (inner_fill_rect_x, inner_fill_rect_y))
-
-        self.surface.blit(inner_surface, (inner_rect_x, inner_rect_y))
-
-    def create_button(self):
-        pass
 
     def blit(self, window):
-        for sprite in self.sprites():
-            sprite.blit(self.surface)
+        window.blit(self.surface, self.blit_pos)
 
 
 
@@ -115,41 +80,49 @@ class Button(Texture):
         window.blit(self.surface, self.blit_pos)
 
 
-class MapCard(Texture):
-    def __init__(self, width, height, inner_surface_fill_color, map_card_name, map_card_data, blit_pos):
-        super().__init__()
-        self.width = width
-        self.height = height
-        self.inner_surface_fill_color = inner_surface_fill_color
+
+
+class GenericMapCardModal(Modal):
+    def __init__(self, inner_rect_color, map_card_name, inner_rect_data, price):
+        self.inner_rect_color = inner_rect_color
+        self.inner_rect_data = inner_rect_data
         self.map_card_name = map_card_name
-        self.map_card_data = map_card_data
-        self.blit_pos = blit_pos
+        super().__init__()
+        self.map_card = MapCard(
+            self.width - 2 * self.x / 2,
+            self.height - 2 * self.y / 4,
+            self.inner_rect_color,
+            self.map_card_name,
+            self.inner_rect_data,
+            (self.x / 2, self.y / 4)
+        )
+        self.button_yes = Button(
+            width_and_height_tuple=(self.width / 4, self.height / 12),
+            background="green",
+            text="Да",
+            text_size=20,
+            text_color="black",
+            blit_pos=(0, self.surface.get_height() - self.height / 12)
+        )
+        self.button_no = Button(
+            width_and_height_tuple=(self.width / 4, self.height / 12),
+            background="red",
+            text="Не",
+            text_size=20,
+            text_color="black",
+            blit_pos=(self.surface.get_width() - self.width / 4, self.surface.get_height() - self.height / 12)
+        )
 
-        self.surface = pygame.Surface((self.width, self.height))
-        self.surface.fill("white")
-        self.inner_fill_surface = pygame.Surface((2 / 3 * self.width, 1 / 5 * self.height))
-        self.inner_fill_surface.fill(self.inner_surface_fill_color)
+        self.add([self.map_card, self.button_no, self.button_yes])
+        self.create_text_and_blit(self.surface, f"Искате ли да купите този имот за {price} $", 30, "white",
+                                  (self.width / 2, self.height / 10))
 
-    def blit_text(self):
-        padding = self.height / 5
-        padding_counter = self.height / 3
-        for word in self.map_card_name.split(" "):
-            self.create_text_and_blit(self.surface, word, 25, (0, 0, 0), (self.width / 2, padding_counter))
-            padding_counter += padding
-        padding_counter += self.height / 5
-
-        for key, value in self.map_card_data.items():
-            if key == "0":
-                self.create_text_and_blit(self.surface, f"Наем: {value} $", 25, (0, 0, 0),
-                                          (self.width / 2, padding_counter))
-                padding_counter += self.height / 8
-            else:
-                self.create_text_and_blit(self.surface, f"{key} {'Къща' if key == '1' else 'Къщи'}", 20, (0, 0, 0),
-                                          (self.width / 4, padding_counter))
-                self.create_text_and_blit(self.surface, f"{value} $", 20, (0, 0, 0),
-                                          (self.width * 3 / 4, padding_counter))
-            padding_counter += padding
 
     def blit(self, window):
-        window.blit(self.surface, self.blit_pos)
+        for sprite in self.sprites():
+            sprite.blit(self.surface)
+        super().blit(window)
+
+
+
 
